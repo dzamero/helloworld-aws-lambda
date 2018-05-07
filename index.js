ffc4c8410edbe955dfedde499a5ef87bcc6ad99f
dzamero <dzamero@gmail.com>
2018-05-07 08:32:04 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+exports.handler = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello World'
+    }),
+  };
+
+  callback(null, response);
+
+};
